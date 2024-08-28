0a3334829815b2238377151265437e3df706b48e
Zengkun Liu <47184822+zkun-liu@users.noreply.github.com>
2024-08-28 13:47:03 +1200

    feat: Implement text processing and chunking strategies
    - Add regex_match function for pattern matching
    - Implement truncate_string function for text trimming
    - Introduce four chunking strategies:
      1. Fixed-size token chunking
      2. Sentence chunking
      3. Window chunking
      4. Semantic chunking

---
+++ smartspace/blocks/sentence_chunk.py
@@ -0,0 +1,61 @@
+from llama_index.core import Document
+from llama_index.core.node_parser import (
+    SentenceSplitter,
+)
+
+from smartspace.core import Block, Config, metadata, step
+from smartspace.enums import BlockCategory
+
+
+@metadata(
+    category=BlockCategory.FUNCTION,
+    description="""
+    Parse text with a preference for complete sentences.
+
+    In general, this class tries to keep sentences and paragraphs together. Therefore
+    compared to the original TokenTextSplitter, there are less likely to be
+    hanging sentences or parts of sentences at the end of the node chunk.
+    
+    Args:
+        chunk_size: The number of tokens to include in each chunk. (default is 200)
+        chunk_overlap: The number of tokens that overlap between consecutive chunks. (default is 10)
+        separator: Default separator for splitting into words. (default is " ")
+        paragraph_separator: Separator between paragraphs. (default is "\\n\\n\\n")
+        secondary_chunking_regex: Backup regex for splitting into sentences.(default is "[^,\\.;]+[,\\.;]?".)
+        
+    Steps: 
+        1: Break text into splits that are smaller than chunk size base on the separators and regex.
+        2: Combine splits into chunks of size chunk_size (smaller than).
+
+    """,
+)
+class SentenceChunk(Block):
+    chunk_size: Config[int] = 200
+    chunk_overlap: Config[int] = 10
+
+    separator: Config[str] = " "
+    paragraph_separator: Config[str] = "\n\n\n"
+
+    secondary_chunking_regex: Config[str] = "[^,.;。？！]+[,.;。？！]?"
+
+    @step(output_name="result")
+    async def sentence_chunk(self, text: str | list[str]) -> list[str]:
+        if isinstance(text, str):
+            doc_text_list = [text]
+        else:
+            doc_text_list = text
+
+        documents = [Document(text=doc_text) for doc_text in doc_text_list]
+
+        splitter = SentenceSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap,
+            separator=self.separator,
+            paragraph_separator=self.paragraph_separator,
+            secondary_chunking_regex=self.secondary_chunking_regex,
+        )
+        try:
+            nodes = splitter.get_nodes_from_documents(documents)
+            return [node.text for node in nodes]
+        except Exception as e:
+            raise RuntimeError(f"Error during chunking: {str(e)}")
